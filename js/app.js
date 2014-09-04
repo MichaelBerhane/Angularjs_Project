9a8c0e60f3d5d7d93c9e9009a4059005d6acda11
Michael Berhane <michael@michaelberhane.org>
2014-09-04 14:20:43 +0100

    new update

--- js/app.js
+++ js/app.js
@@ -52,8 +52,16 @@ app.controller('PanelController', function(){
 		return this.tab === checkTab;
 	}
 
-
 });	
+
+app.controller('ReviewController', function(){
+	this.review = {};
+
+	this.addReview = function(product){
+		product.reviews.push(this.review)
+	}
+	
+})
 
 
 
